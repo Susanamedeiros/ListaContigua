21e478838e4885f1b47a64479a847bc566d1a7e6
Susanamedeiros <susiweb2009@gmail.com>
2013-07-18 17:53:52 -0300

    primeira versao do projeto

---
+++ src/listcont/Main.java
@@ -0,0 +1,20 @@
+package listcont;
+
+public class Main {
+	public static void main(String[] args) {
+		TListCont1 l1 = new TListCont1();
+	    int i, tam1=4, valor;
+	    System.out.println("Digite o tamanho da lista");
+	     /* cin >> taml; NAO SEI FAZER UM EQUIVALENTE. */
+	     l1.TListCont1(tam1);
+	    System.out.println("Digite um numero inteiro");
+	    /* for (i=0; i<taml; i++)
+	    {
+	    	cin >> valor;  NAO SEI FAZER UM EQUIVALENTE
+	        l1.insereUlt(valor);
+	    }*/
+	    
+	    //TODO Procurar sobre a classe Scanner 
+	    l1.imprime();
+	}
+}
---
+++ src/listcont/TListCont1.java
@@ -0,0 +1,81 @@
+package listcont;
+
+public class TListCont1 {
+	        int m;
+	        int ultimo;
+	        float[] x;
+
+	        
+	        
+	    public void TListCont(int tam){
+	    	m = tam;
+		    ultimo = -1;
+		    x = new float[tam];
+	    }
+	    public float consulta(int k) throws  IndexOutOfBoundsException{
+	    	if(k >= 0 && k <= ultimo)
+		        return x[k];
+		    else
+		    {
+		        throw new IndexOutOfBoundsException("Indice invalido!");
+		    }
+	    }
+	    public void atribui(int k, float val){
+		    if(k >= 0 && k <= ultimo)
+		        x[k] = val;
+		    else
+		    {
+		        System.out.println("Indice invalido!");
+		    }
+	    }
+	    public void insereK(int k, float val){
+		    if(ultimo == m-1)
+		    {
+		        System.out.println("Vetor cheio!");
+		    }
+
+		    if(k >= 0 && k <= ultimo)
+		    {
+		        for(int i = ultimo; i >= k; i--)
+		            x[i+1] = x[i];
+		        x[k] = val;
+		        ultimo = ultimo + 1;
+		    }
+		    else
+		    {
+		        System.out.println("Indice invalido!");
+		    }
+	    }
+	    public void insereUlt(float val){
+	        	if(ultimo == m-1)
+	    	    {
+	    	        System.out.println("Vetor cheio!");
+	    	    }
+	    	    ultimo = ultimo + 1;
+	    	    x[ultimo] = val;
+	        }
+	    public void eliminaK(int k){
+	    	    if(k >= 0 && k <= ultimo)
+	    	    {
+	    	        for(int i = k; i <= ultimo-1; i++)
+	    	            x[i] = x[i+1];
+	    	        ultimo = ultimo - 1;
+	    	    }
+	    	    else
+	    	    {
+	    	        System.out.println("Indice invalido!");
+	    	    }
+	        }
+	    public void eliminaUlt(){
+	    	    if(ultimo == -1)
+	    	    {
+	    	        System.out.println("Lista vazia!");
+	    	    }
+	    	    ultimo = ultimo - 1;
+	        }
+	    public void imprime(){
+	    	    int i;
+	    	    for (i=0; i<=ultimo; i++)
+	    	        System.out.println(x[i] + "  ");
+	        }
+}
